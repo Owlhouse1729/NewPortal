fe90e213b1714d86b4e57ca6913796dd03f0878f
gitakmin <takujomu@gmail.com>
2022-07-27 02:14:09 +0900

    heroku settings

--- NewPortal/settings.py
+++ NewPortal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-adq=ie#zr09vm0233g%mch+)diqdaucc90t15cuj+_s-+!9g65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
